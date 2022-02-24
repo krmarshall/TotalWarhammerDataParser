8904519c365c5598241cda2e8c07907a112602b1
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-02-23 23:14:15 -0700

    Set up scripts to fully build

--- scripts/rpfmExtractTables.bat
+++ scripts/rpfmExtractTables.bat
@@ -1,6 +1,8 @@
 :: Clear any data from previous extracts
-RD /S /Q D:\GitHub\TotalWarhammerDataParser\extracted_files\vanilla\db
-cd ../rpfm
+RD /S /Q D:\GitHub\TotalWarhammerDataParser\extracted_files\vanilla
+cd ../
+mkdir extracted_files\vanilla
+cd ./rpfm
 
 :: Extract tables from packfiles
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/character_skill_node_sets_tables/data__"
@@ -10,15 +12,15 @@ rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e ".
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/character_skill_node_links_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/character_skill_nodes_skill_locks_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/effects_tables/data__"
-rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/unit_abilities_tables/data__"
-rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/effect_bonus_value_unit_ability_junctions_tables/data__"
+:: rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/unit_abilities_tables/data__"
+:: rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/effect_bonus_value_unit_ability_junctions_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/character_skill_level_to_effects_junctions_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/ancillary_types_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/ancillaries_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/data.pack" packfile -e "../extracted_files/vanilla" - "db/character_skill_level_to_ancillaries_junctions_tables/data__"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/local_en.pack" packfile -e "../extracted_files/vanilla" - "text/db/character_skills__.loc"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/local_en.pack" packfile -e "../extracted_files/vanilla" - "text/db/effects__.loc"
-rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/local_en.pack" packfile -e "../extracted_files/vanilla" - "text/db/unit_abilities__.loc"
+:: rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/local_en.pack" packfile -e "../extracted_files/vanilla" - "text/db/unit_abilities__.loc"
 rpfm_cli.exe -g warhammer_2 -p "../game_source/vanilla/local_en.pack" packfile -e "../extracted_files/vanilla" - "text/db/ancillaries__.loc"
 
 
@@ -30,13 +32,15 @@ rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/character_sk
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/character_skill_node_links_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/character_skill_nodes_skill_locks_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/effects_tables/data__"
-rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/unit_abilities_tables/data__"
-rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/effect_bonus_value_unit_ability_junctions_tables/data__"
+:: rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/unit_abilities_tables/data__"
+:: rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/effect_bonus_value_unit_ability_junctions_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/character_skill_level_to_effects_junctions_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/ancillary_types_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/ancillaries_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/db/character_skill_level_to_ancillaries_junctions_tables/data__"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/text/db/character_skills__.loc"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/text/db/effects__.loc"
-rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/text/db/unit_abilities__.loc"
+:: rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/text/db/unit_abilities__.loc"
 rpfm_cli.exe -g warhammer_2 table -e "../extracted_files/vanilla/text/db/ancillaries__.loc"
+
+EXIT /B 0
