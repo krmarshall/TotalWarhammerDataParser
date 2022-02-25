b8fe70845a155b1a095e91db1910c4cfb93c407d
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-02-24 17:10:39 -0700

    More stapling, aim for immutable objects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { parseVanillaFiles } from './parseFiles.js';
 import { stapleVanillaTables } from './stapleTables.js';
 
+console.time('parseVanilla');
 parseVanillaFiles()
   .then(() => {
+    console.timeEnd('parseVanilla');
+
+    console.time('stapleVanilla');
     stapleVanillaTables();
+    console.timeEnd('stapleVanilla');
   })
   .catch((error) => {
     console.log(error);
--- src/stapleTables.js
+++ src/stapleTables.js
@@ -2,23 +2,37 @@ import fse from 'fs-extra';
 import ancillariesPrune from './pruneLists/ancillariesPrune.js';
 
 const stapleVanillaTables = () => {
-  const ancillaries = JSON.parse(fse.readFileSync('./parsed_files/vanilla/db/ancillaries_tables.json', 'utf-8'));
+  // Think I dont actually need ancillaries tables or locs?
+  let ancillaries = JSON.parse(fse.readFileSync('./parsed_files/vanilla/db/ancillaries_tables.json', 'utf-8'));
   const ancillaryTypes = JSON.parse(fse.readFileSync('./parsed_files/vanilla/db/ancillary_types_tables.json', 'utf-8'));
   const ancillariesLoc = JSON.parse(fse.readFileSync('./parsed_files/vanilla/text/db/ancillaries.json', 'utf-8'));
-  const ancillaries_AncillaryTypes = staple_Ancillaries_AncillaryTypes(ancillaries, ancillaryTypes, ancillariesLoc);
+  ancillaries = [...staple_ancillaries_ancillaryTypes(ancillaries, ancillaryTypes, ancillariesLoc)];
 
-  const characterSkillLevelToAncillariesJunction = JSON.parse(
+  let characterSkillLevelToAncillariesJunction = JSON.parse(
     fse.readFileSync('./parsed_files/vanilla/db/character_skill_level_to_ancillaries_junctions_tables.json', 'utf-8')
   );
-  const characterSkillLevelAncillary = staple_characterSkillLevelToAncillariesJunction_Ancillaries(
-    characterSkillLevelToAncillariesJunction,
-    ancillaries_AncillaryTypes
-  );
+  characterSkillLevelToAncillariesJunction = [
+    ...staple_characterSkillLevelToAncillariesJunction_ancillaries(
+      characterSkillLevelToAncillariesJunction,
+      ancillaries
+    ),
+  ];
 
-  fse.outputFileSync('./test/test.json', JSON.stringify(characterSkillLevelAncillary, null, 2));
+  let effects = JSON.parse(fse.readFileSync('./parsed_files/vanilla/db/effects_tables.json', 'utf-8'));
+  const effectsLoc = JSON.parse(fse.readFileSync('./parsed_files/vanilla/text/db/effects.json', 'utf-8'));
+  effects = [...staple_effects_effectsLoc(effects, effectsLoc)];
+
+  let characterSkillLevelToEffectsJunction = JSON.parse(
+    fse.readFileSync('./parsed_files/vanilla/db/character_skill_level_to_effects_junctions_tables.json', 'utf-8')
+  );
+  characterSkillLevelToEffectsJunction = [
+    ...staple_effects_characterSkillLevelToEffectsJunction(effects, characterSkillLevelToEffectsJunction),
+  ];
+
+  fse.outputFileSync('./test/test.json', JSON.stringify(characterSkillLevelToEffectsJunction, null, 2));
 };
 
-const staple_Ancillaries_AncillaryTypes = (ancillaries, ancillaryTypes, ancillariesLoc) => {
+const staple_ancillaries_ancillaryTypes = (ancillaries, ancillaryTypes, ancillariesLoc) => {
   const stapledTable = ancillaries.map((ancillary) => {
     ancillariesPrune.forEach((prune) => {
       delete ancillary[prune];
@@ -39,23 +53,63 @@ const staple_Ancillaries_AncillaryTypes = (ancillaries, ancillaryTypes, ancillar
     });
     ancillary.text = locText.text;
 
-    return ancillary;
+    return { ...ancillary };
   });
   return stapledTable;
 };
 
-const staple_characterSkillLevelToAncillariesJunction_Ancillaries = (
+const staple_characterSkillLevelToAncillariesJunction_ancillaries = (
   characterSkillLevelToAncillariesJunction,
   ancillaries
 ) => {
-  const stapledTable = characterSkillLevelToAncillariesJunction.map((characterSkillLevelToAncillaryJunction) => {
+  const stapledTable = characterSkillLevelToAncillariesJunction.map((record) => {
     const relatedAncillary = ancillaries.find((ancillary) => {
-      return ancillary.key === characterSkillLevelToAncillaryJunction.granted_ancillary;
+      return ancillary.key === record.granted_ancillary;
     });
-    characterSkillLevelToAncillaryJunction.granted_ancillary = relatedAncillary;
+    record.granted_ancillary = { ...relatedAncillary };
 
-    return characterSkillLevelToAncillaryJunction;
+    return { ...record };
   });
+  return stapledTable;
+};
+
+const staple_effects_effectsLoc = (effects, effectsLoc) => {
+  const stapledTable = effects.map((effect) => {
+    const locDescription = effectsLoc.find((effectLoc) => {
+      return effectLoc.key === `effects_description_${effect.effect}`;
+    });
+    effect.description = locDescription.text;
+    return { ...effect };
+  });
+  return stapledTable;
+};
+
+const staple_effects_characterSkillLevelToEffectsJunction = (effects, characterSkillLevelToEffectsJunction) => {
+  const stapledTable = characterSkillLevelToEffectsJunction.map((record) => {
+    const relatedEffect = effects.find((effect) => {
+      return effect.effect === record.effect_key;
+    });
+
+    // Most skills have an effect that increases or decreases agent action chances, if I dont want to display these in the future uncomment these sections
+    // if (
+    //   (relatedEffect.effect === 'wh_main_effect_agent_action_success_chance_enemy_skill' &&
+    //     relatedEffect.priority === '0') ||
+    //   (relatedEffect.effect === 'wh_main_effect_agent_action_success_chance_skill' && relatedEffect.priority === '0')
+    // ) {
+    //   return;
+    // }
+    delete relatedEffect.icon_negative;
+    delete record.effect_key;
+    relatedEffect.key = relatedEffect.effect;
+    record.effect = { ...relatedEffect };
+    delete record.effect.effect;
+    return { ...record };
+  });
+
+  // const filteredTable = stapledTable.filter((record) => {
+  //   return record != null;
+  // });
+  // return filteredTable;
   return stapledTable;
 };
 
