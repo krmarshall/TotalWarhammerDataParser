14d341b2bba3842992173c8f7f933693f6d394d4
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-03-22 19:39:43 -0600

    Play with workers

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
-import { extractPackfile, extractTsv } from './rpfmFunctions.js';
-import { parseVanillaFiles, parseVanilla3Files } from './parseFiles.js';
-import { stapleTables, stapleTables3 } from './stapleTables.js';
+import { Worker } from 'worker_threads';
 import { v2DbList, v2LocList } from './extractLists/vanilla2.js';
-import { sfo2DbList, sfo2LocList } from './extractLists/sfo2.js';
 
-console.time('total');
-// Vanilla 2
-extractPackfile('vanilla', 'data', 'local_en', v2DbList, v2LocList, 'warhammer_2')
-  .then(() => extractTsv('vanilla', 'warhammer_2'))
-  .then(() => parseVanillaFiles())
-  .then(() => {
-    stapleTables('vanilla');
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const workerVanilla2 = new Worker('./src/worker.js', {
+  workerData: { folder: 'vanilla', dbPackName: 'data', locPackName: 'local_en', dbList: v2DbList, locList: v2LocList, game: 'warhammer_2' },
+});
+workerVanilla2.on('error', (error) => {
+  console.log(error);
+});
 
-// Vanilla 3
-extractPackfile('vanilla3', 'data', 'local_en', v2DbList, v2LocList, 'warhammer_3')
-  .then(() => extractTsv('vanilla3', 'warhammer_3'))
-  .then(() => parseVanilla3Files())
-  .then(() => {
-    stapleTables3('vanilla3');
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// SFO 2
-extractPackfile('sfo', 'steel_faith_overhaul_2', 'steel_faith_overhaul_2', sfo2DbList, sfo2LocList, 'warhammer_2')
-  .then(() => extractTsv('sfo', 'warhammer_2'))
-  .catch((error) => {
-    console.log(error);
-  });
+const workerVanilla3 = new Worker('./src/worker.js', {
+  workerData: {
+    folder: 'vanilla3',
+    dbPackName: 'data',
+    locPackName: 'local_en',
+    dbList: v2DbList,
+    locList: v2LocList,
+    game: 'warhammer_3',
+  },
+});
+workerVanilla3.on('error', (error) => {
+  console.log(error);
+});
