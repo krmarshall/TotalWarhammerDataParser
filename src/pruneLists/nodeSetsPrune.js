9a2162eb7bec0a8bc0bfd61daced90814e73ea85
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-04-29 19:31:41 -0600

    Prune ritual heroes with no skill tree

--- src/pruneLists/nodeSetsPrune.js
+++ src/pruneLists/nodeSetsPrune.js
@@ -1,5 +1,6 @@
 const nodeSetsPrune = [
   'wh2_pro08_skill_node_neu_felix',
+  'wh2_pro08_skill_node_neu_gotrek',
 
   'wh2_dlc13_lzd_slann_mage_priest_fire_horde',
   'wh2_dlc13_lzd_slann_mage_priest_high_horde',
@@ -9,6 +10,15 @@ const nodeSetsPrune = [
   'wh2_dlc13_lzd_kroxigor_ancient_horde',
   'wh2_dlc13_lzd_red_crested_skink_chief_horde',
   'wh2_dlc13_lzd_slann_mage_priest_horde',
+
+  'wh2_dlc09_tmb_necrotect_ritual',
+
+  'wh2_dlc10_hef_shadow_walker',
+
+  'wh2_main_skv_plague_priest_ritual',
+  'wh2_main_skv_warlock_engineer_ritual',
+
+  'wh3_main_nur_cultist_plague_ritual',
 ];
 
 export default nodeSetsPrune;
