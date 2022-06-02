5fc634f789ce491723cf39cdcc53094b996b86e6
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-06-02 16:46:28 -0600

    Fix for levels being 0 indexed in criteria table

--- src/stapleFunctions3/characterSkills_characterSkillsToLevelReachedCriterias.js
+++ src/stapleFunctions3/characterSkills_characterSkillsToLevelReachedCriterias.js
@@ -2,10 +2,10 @@ const characterSkills_characterSkillsToLevelReachedCriterias = (characterSkills,
   const stapledTable = characterSkills.map((skill) => {
     const relatedCriteria = characterSkillsToLevelReachedCriterias.find((criteria) => criteria.skill_key === skill.key);
     if (relatedCriteria !== undefined) {
-      if (relatedCriteria.level === '1') {
+      if (relatedCriteria.level === '0') {
         skill.points_on_creation = 1;
       } else {
-        skill.levels[relatedCriteria.rank - 1].auto_unlock_at_rank = parseInt(relatedCriteria.level);
+        skill.levels[relatedCriteria.rank - 1].auto_unlock_at_rank = parseInt(relatedCriteria.level) + 1;
       }
     }
 
