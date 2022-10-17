1a9ae95ca7d7e9daf4ca33b008badd02350120e3
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-10-16 23:41:41 -0600

    Fix: Skill blocking

--- src/stapleFunctions/characterSkillNodes_characterSkillNodesSkillLocks.js
+++ src/stapleFunctions/characterSkillNodes_characterSkillNodesSkillLocks.js
@@ -1,14 +1,14 @@
 const characterSkillNodes_characterSkillNodesSkillLocks = (characterSkillNodes, characterSkillNodesSkillLocks) => {
   const stapleTables = characterSkillNodes.map((node) => {
     const relatedSkillLocks = characterSkillNodesSkillLocks.filter((skillLock) => {
-      return skillLock.character_skill_node === node.key;
+      return skillLock.character_skill === node.character_skill_key;
     });
     relatedSkillLocks.forEach((skillLock) => {
       if (node.levels[skillLock.level - 1].blocks_character_skill_key === undefined) {
         node.levels[skillLock.level - 1].blocks_character_skill_key = [];
       }
-      if (!node.levels[skillLock.level - 1].blocks_character_skill_key.includes(skillLock.character_skill)) {
-        node.levels[skillLock.level - 1].blocks_character_skill_key.push(skillLock.character_skill);
+      if (!node.levels[skillLock.level - 1].blocks_character_skill_key.includes(skillLock.character_skill_node)) {
+        node.levels[skillLock.level - 1].blocks_character_skill_key.push(skillLock.character_skill_node);
       }
     });
 
