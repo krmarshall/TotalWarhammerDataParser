2330088aa5442f7cbac911524585d7a7dba3affa
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-11-09 15:20:39 -0700

    Shuffle effects around to remove unnecessary properties/nesting

--- src/stapleFunctionsTechs/techs_techEffectsJunc.js
+++ src/stapleFunctionsTechs/techs_techEffectsJunc.js
@@ -11,18 +11,23 @@ const techs_techEffectsJunc = (techs, techEffectsJunc) => {
     const relatedRecords = techEffectsMap[tech.key];
 
     if (relatedRecords !== undefined) {
+      tech.effects = [];
       relatedRecords.forEach((relatedRecord) => {
-        // If the record has effects instead of effect prop, it has already been parsed and doesnt need to have props shuffled around
-        if (relatedRecord.effects === undefined) {
-          relatedRecord.effect.value = relatedRecord.value;
-          relatedRecord.effect.effect_scope = relatedRecord.effect_scope;
-          delete relatedRecord.value;
-          delete relatedRecord.effect_scope;
-          relatedRecord.effects = { ...relatedRecord.effect };
-          delete relatedRecord.effect;
+        const tempObj = {
+          key: relatedRecord.effect.key,
+          description: relatedRecord.effect.description,
+          value: relatedRecord.effect.value,
+          icon: relatedRecord.effect.icon,
+          priority: relatedRecord.effect.priority,
+          is_positive_value_good: relatedRecord.effect.is_positive_value_good,
+        };
+
+        if (relatedRecord.effect.related_abilities !== undefined) {
+          tempObj.related_abilities = JSON.parse(JSON.stringify(relatedRecord.effect.related_abilities));
         }
+
+        tech.effects.push(tempObj);
       });
-      tech.effects = relatedRecords;
 
       tech.effects.sort((a, b) => a.priority - b.priority);
     }
