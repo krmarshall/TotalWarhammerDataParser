aca96cc0f564964074cbe233d8f5882ca2816dd9
krmarshall <59742056+krmarshall@users.noreply.github.com>
2023-05-28 18:47:51 -0600

    Fix: more generalized cleanNodeSetKey regex

--- src/utils/cleanNodeSetKey.ts
+++ src/utils/cleanNodeSetKey.ts
@@ -3,7 +3,7 @@ const cleanNodeSetKey = (key: string) => {
   const keyName = key.split(
     /node_set_|_skill_node_(?!set_)|variety_agent_subtype_|wh3_dlc20_|rad_wh3_part3_main_unit_|^kou_skill_|^dust_skill_|^wh3_pro11_/
   );
-  const cleanedKeyName = keyName[keyName.length - 1].replace(/_skill_node$|_skills$|_node_set$|_nodeset$|_node$|_set$/, '');
+  const cleanedKeyName = keyName[keyName.length - 1].replace(/_(skills?)?(_?nodes?)?(_?sets?)?$/, '');
   return cleanedKeyName;
 };
 
